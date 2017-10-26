e9ab63a2504ee5293902eabecaf94e22c7eb88da
Alex Smith <alexsmith716@outlook.com>
2017-10-26 15:49:49 -0400

    Got some cool bootstrap stuff working on homepage.

--- client/modules/home/components/HomeHeader.js
+++ client/modules/home/components/HomeHeader.js
@@ -10,8 +10,25 @@ import style from './style.css';
 const HomeHeader = (props: { showHeaderText: true }) => (
   <Header className={style.colorMarginStyle}>
     {props.showHeaderText && (
-      <HeaderText>
+      <HeaderText className={css.coloredText}>
+        <hr />
         Find the best jobs<br />at the best tech companies
+        <hr />
+        <br />
+        <div className={`row ${css.colItems}`}>
+          <div className={`col-md-4 ${css.colItem}`}>
+            <span className="glyphicon glyphicon-headphones" />
+            <span className={css.text}>{'It.'}</span>
+          </div>
+          <div className={`col-md-4 ${css.colItem}`}>
+            <span className="glyphicon glyphicon-glass" />
+            <span className={css.text}>{'Just.'}</span>
+          </div>
+          <div className={`col-md-4 ${css.colItem}`}>
+            <span className="glyphicon glyphicon-thumbs-up" />
+            <span className={css.text}>{'Works.'}</span>
+          </div>
+        </div>
       </HeaderText>
     )}
     <HomeSearch />
